9697e1c511339093b411d11c2813695d92cc48ff
supernovatux <thulashitharan.d@gmail.com>
2022-08-05 01:35:15 +0530

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use std::io;
 use std::io::prelude::*;
 pub mod get_pass;
+pub mod setup_chrome_driver;
 
 pub fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
-    write!(stdout,"Press any key to continue.....").unwrap();
+    write!(stdout, "Press any key to continue.....").unwrap();
     stdout.flush().unwrap();
 
     let _ = stdin.read(&mut [1u8]).unwrap();
